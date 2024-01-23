7524e6b4c748ec0532d9681e8c1e35c015427e46
Wolverinebaba <110099229+Wolverinebaba@users.noreply.github.com>
2024-01-23 11:16:40 +0000

    container config done

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,18 @@
+ const accountId=325494
+ let accountEmail="harsh@google.com"
+ var accountpassword="12345"
+ accountCity="sultanpur"
+
+
+ console.log(accountId)
+
+
+ // to change 
+
+
+ accountEmail="harshhh@gmail.com"
+ accountpassword= "222222"
+
+ console.table([accountId,accountEmail,accountpassword])
+
+ /* prefer ot to use "var " Beacus of issue in block scope and functional scope*/
---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict"// treat all jscodes as newer version
+
+//alert(3+3) we are using node js not browser
+
+//console.log(3+3); console.log("harsh") code readable hoa chahiye 
+
+//  premitive data type--------------------------
+
+ //1- number= 2 to power 53
+ //2- string=""
+ //3- bigint=
+ //4- null= standalone value
+ //5- undefiend=  without any value
+ //6-  sybole=  to find uniqueness
+
+ console.log(typeof null);
